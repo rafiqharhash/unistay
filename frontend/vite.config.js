5028c549f7b27842231e60cfb0025a7d4e70c980
Rafiq Harhash <rafeqnusr@icloud.com>
2026-06-10 21:31:34 +0200

    UPDATE

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: process.env.PORT ? parseInt(process.env.PORT) : 4173,
+    host: true,
+  },
   server: {
     port: 5173,
     proxy: {
